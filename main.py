4e5caab40ee2f301bc85fe633fa9989bdcb54c0d
tanevitch <lubatane@gmail.com>
2024-01-31 17:24:52 -0300

    falta el pattern para barrio

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ import copy
 
 nlp = spacy.load('es_core_news_lg')
 
-# doc=nlp('Se trata de un galpón de 3 frentes, ubicado sobre Av. 7 N° 1540/1550 a escasos metros del triangulo de Bernal, en zonificación R3  (permitiendo industrias de actividad inocua) de excelente acceso y de salida rápida a autopista Buenos Aires la Plata,diferentes puntos del conurbano como así también a Capital Federal.Galpón N° 1540:Implantado sobre dos lotes de terreno.En su primera mitad de 14,80 mts. x  mts. x 25,70 mts. Sup. Cubierta: 380,3.')
-# for token in doc:
-#     print(token.text, token.pos_, token.dep_)
-# displacy.serve(doc, style='dep')
-
 #-----------------------------------------------------------
 # DEPENDENCY MATCHER
 #-----------------------------------------------------------
@@ -25,12 +20,12 @@ fot = [
 ]
 frentes = [
     [
-        {'RIGHT_ID': 'frentes', 'RIGHT_ATTRS': {'LOWER': 'frentes'}}, 
-        {'LEFT_ID': 'frentes', 'REL_OP': '>', 'RIGHT_ID': 'num', 'RIGHT_ATTRS': {'DEP': 'nummod'}} 
+        {'RIGHT_ID': 'frentes', 'RIGHT_ATTRS': {'LOWER': {"IN": ['frentes', 'frente']}}}, 
+        {'LEFT_ID': 'frentes', 'REL_OP': '>', 'RIGHT_ID': 'num', 'RIGHT_ATTRS': {'DEP': {"IN": ['nummod', 'amod']}}} 
     ]
 ]
-dep_matcher.add('FOT', patterns=fot)
 dep_matcher.add('FRENTES', patterns=frentes)
+dep_matcher.add('FOT', patterns=fot)
 
 #-----------------------------------------------------------
 # MATCHER
@@ -42,12 +37,21 @@ esquina = [
 pileta = [
     {'LOWER': {'IN': ['piscina', 'pileta']}}, 
 ]
-dimension = [{'LIKE_NUM': True}, {'LOWER': 'x'}, {'LIKE_NUM': True}]
+dimension_x = [{'LIKE_NUM': True}, {'LOWER': 'x'}, {'LIKE_NUM': True}]
+
+dimension_larga = [
+    {'LIKE_NUM': True}, 
+    {'LOWER': {"IN": ["mts","m","metros"]}, "OP":"?"}, 
+    {'LIKE_NUM': True},
+    {'LOWER': {"IN": ["mts","m","metros"]}, "OP":"?"},
+]
+
+
 dir_altura = [
     {'LOWER': {'IN': ['calle', 'avenida', 'av', 'diagonal', 'diag']}, 'OP':'?'},   
     {'TEXT': '.', 'OP':'?'},  
     {'POS': 'PROPN', 'OP': '+'},  
-    {'LOWER': 'al'}, 
+    {'LOWER': 'al', "OP": "?"}, 
     {'LIKE_NUM': True}    
 ]
 dir_nro = [
@@ -67,7 +71,7 @@ dir_interseccion = [
     {'LOWER': {'IN': ['calle', 'avenida', 'av', 'diagonal', 'diag']}, 'OP':'?'},   
     {'TEXT': '.', 'OP':'?'}, 
     {'POS': {'IN': ['PROPN', 'NUM']}, 'OP': '+'},  
-    {'LOWER': 'y'},  
+    {'LOWER': {"IN": ["y", "esquina", "esq"]}},  
     {'LOWER': {'IN': ['calle', 'avenida', 'av', 'diagonal', 'diag']}, 'OP':'?'},   
     {'TEXT': '.', 'OP':'?'},      
     {'POS': {'IN': ['PROPN', 'NUM']}, 'OP': '+'},     
@@ -88,7 +92,18 @@ dir_entre = [
 irregular= [
     {'LOWER': 'irregular'}
 ]
-matcher.add('DIMENSION', [dimension])
+fot=[
+    {'TEXT': {'IN':['FOT', 'fot', 'F.O.T', 'f.o.t', 'Fot', 'F.o.t']}},
+    {'LOWER':  {'IN':['res', 'residencial']}, "OP": "?"},
+    {"IS_PUNCT": True, "OP":"?"},
+    {"LIKE_NUM": True},
+    {"IS_PUNCT": True, "OP":"?"},
+    {'TEXT': {'IN':['FOT', 'fot', 'F.O.T', 'f.o.t', 'Fot', 'F.o.t']}, "OP": "?"},
+    {'LOWER': {'IN':['comercial', 'com', 'industrial']}, "OP": "?"},
+    {"IS_PUNCT": True, "OP":"?"},
+    {"LIKE_NUM": True, "OP": "?"}
+]
+matcher.add('DIMENSION', [dimension_x, dimension_larga])
 matcher.add('DIR_NRO', [dir_nro])
 matcher.add('DIR_ALTURA', [dir_altura])
 matcher.add('DIR_INTERSECCION', [dir_interseccion])
@@ -97,6 +112,7 @@ matcher.add('DIR_LOTE', [dir_lote])
 matcher.add('PILETA', [pileta])
 matcher.add('ESQUINA', [esquina])
 matcher.add('IRREGULAR', [irregular])
+matcher.add('FOT', [fot])
 
 
 def add_spaces(match):
@@ -178,10 +194,12 @@ def matches_dep(doc):
     }
     matches_dep = dep_matcher(doc)
     for match_id, token_ids in matches_dep:
-        for token_id in token_ids:
+        palabra= []
+        for token_id in sorted(token_ids):
             token = doc[token_id]
-        result[nlp.vocab.strings[match_id]].append(token.text)
-        prev_result[nlp.vocab.strings[match_id]].append(token.text)
+            palabra.append(token.text)
+        result[nlp.vocab.strings[match_id]].append(' '.join(palabra))
+        prev_result[nlp.vocab.strings[match_id]].append(' '.join(palabra))
     return prev_result
 
 def merge(dic1, dic2):
@@ -315,7 +333,12 @@ for index, row in input.iterrows():
         rta= prev_result[respuesta]
         if key_metrica == "medidas":
             esperada= normalizar_dimensiones(esperada)
-        if (nlp(rta).similarity(nlp(esperada)) > 0.5):
+        if key_metrica in ["direccion","fot", "medidas", "barrio", "frentes"]:
+            correcta= nlp(rta).similarity(nlp(esperada)) > 0.5
+        elif key_metrica in [ "irregular", "esquina", "pileta"]:
+            correcta= True if esperada==rta or (rta != "" and esperada == True) else False
+
+        if correcta:
             metricas[key_metrica]["tp"]+=1
         else:
             metricas[key_metrica]["error"].append({
